47e853e7a398912bdc2a3196b36478234d2c24f0
Francisco M. Miranda <fmanuelos@gmail.com>
2022-07-11 12:34:49 -0400

    Bug Fix: Fixed the Responsive Bug

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -77,7 +77,7 @@ const NavDropdownItem = styled(NavDropdown.Item)`
 `;
 
 const NavigationBar = () => (
-  <NavbarStyled bg="dark" expand="lg" role="navigation">
+  <NavbarStyled bg="dark" role="navigation">
     <NavbarContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
