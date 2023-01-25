08d1fad8ba6bc07c6eed9b94e2ed2558c708e987
Joon Lee <joon.lee2@nih.gov>
2023-01-25 14:04:20 -0500

    Removed premature reference to Node class from MultiYamlDataRetriever

--- server/lib/DataConnection/MultiYamlDataRetriever.js
+++ server/lib/DataConnection/MultiYamlDataRetriever.js
@@ -1,5 +1,4 @@
 const DataRetriever = require('./DataRetriever');
-const Node = require('./../../lib/Node');
 const Property = require('./../../lib/Property');
 const fs = require('fs');
 const $RefParser = require('@apidevtools/json-schema-ref-parser');
